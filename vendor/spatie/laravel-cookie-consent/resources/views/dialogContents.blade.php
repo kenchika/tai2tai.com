caa252cb4f84a329a7dfb5e328109d9bd5ccceef
laurent <Laurent.gastaud@etu.uca.fr>
2019-07-04 18:20:09 +0800

    ignore

---
+++ vendor/spatie/laravel-cookie-consent/resources/views/dialogContents.blade.php
@@ -0,0 +1,20 @@
+<div class="js-cookie-consent cookie-consent">
+  <div class="container-fluid fixed-bottom  shade ">
+    <div class="container shade bg-white py-3 ">
+      <div class="row justify-content-center">
+        <div class="col-md-5 col-sm-12 text-center cookie-consent__message mdc-typography--body2 my-auto " >
+
+            {!! trans('cookieConsent::texts.message') !!}
+
+        </div>
+        <div class="col-md-4 col-sm-12 text-center" >
+          <button class="js-cookie-consent-agree cookie-consent__agree mdc-button mdc-button--unelevated">
+            <span class="mdc-button__label ">  {{ trans('cookieConsent::texts.agree') }}</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
